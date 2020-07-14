14c23dff38a14ed2f677849d4c6c809d1357392d
4sMarcoPorto <marcoporto2011@hotmail.com>
2020-07-14 19:56:37 +0100

    update

--- Electron/Tray/index.js
+++ Electron/Tray/index.js
@@ -1,12 +1,15 @@
 const ipc = require('electron').ipcMain;
 const qrcode  = require("qrcode");
-
-const TrayWindow = require("electron-tray-window");
-const { app } = require("electron");
-
 const path = require("path");
 
+const TrayWindow = require("electron-tray-window");
+const { app,BrowserWindow } = require("electron");
+
+//render tray
 app.on("ready", () => {
+  
+
+
   TrayWindow.setOptions({
     trayIconPath: path.join("resources/assets/icon@3x.png"),
     windowUrl: `file://${path.join(__dirname, "resources/view.html")}`,
@@ -16,6 +19,7 @@ app.on("ready", () => {
 });
 
 
+//Generate qrcode from string provide by ipc
 async function QrCodeGenerator(string) {
   qrcode.toFile('./resources/view-engine/qr.png', string, {
     color: {
@@ -24,12 +28,15 @@ async function QrCodeGenerator(string) {
     }
   }, function (err) {
     if (err) throw err
-    console.log('done')
+      console.log("[QR-Code]:",200)
   })
 }
 
+
+//ipc listen runQrCodeGenerator then calls QrCodeGenerator function
 ipc.on('runQrCodeGenerator', (event, args) => {
- console.log(args);
+ console.log('[String-Converted]:',args);
+
  QrCodeGenerator(args).catch(error => console.error(error.stack));
  event.sender.send('QrCode200',true)
 });
