5a7996921a8d39cf0d7b1a3397d0ba57214ccc3f
4sMarcoPorto <marcoporto2011@hotmail.com>
2020-07-18 20:06:45 +0100

    update

--- Electron/Tray/index.js
+++ Electron/Tray/index.js
@@ -3,37 +3,46 @@ const qrcode  = require("qrcode");
 const path = require("path");
 
 const TrayWindow = require("electron-tray-window");
-const { app,BrowserWindow } = require("electron");
+const { app } = require("electron");
 
 //render tray
 app.on("ready", () => {
 
+
   TrayWindow.setOptions({
-    trayIconPath: path.join("resources/assets/icon@3x.png"),
+    trayIconPath: path.join("resources/assets/images/icon.png"),
     windowUrl: `file://${path.join(__dirname, "resources/view.html")}`,
     width:300,
-    height:350
+    height:350,
+    margin_x : -100, 
+    margin_y : 30
   });
 });
 
 //Generate qrcode from string provide by ipc
 async function QrCodeGenerator(string) {
-  qrcode.toFile('./resources/view-engine/qr.png', string, {
+  const qrCodePath = "./resources/view-engine/qr.png"
+  qrcode.toFile(qrCodePath, string, {
     color: {
       dark: '#000000ff',
       light: '#ffffffff' 
     }
   }, function (err) {
-    if (err) throw err
-      console.log("[QR-Code]:",200)
+    if (err) throw err //cant generate qrcode....return error
+      console.log('[QrCode] ---- ','\x1b[32m','[OK]','\x1b[0m',', at ' + qrCodePath);
   })
 }
 
 //ipc listen runQrCodeGenerator then calls QrCodeGenerator function
 ipc.on('runQrCodeGenerator', (event, args) => {
- console.log('[String-Converted]:',args);
+  console.log('\n \n \n')
+  console.log('[String-Converted] ---- ',args);
 
- QrCodeGenerator(args).catch(error => console.error(error.stack));
- event.sender.send('QrCode200',true)
+  QrCodeGenerator(args).catch(error => console.error(error.stack));
+  event.sender.send('QrCode200',true)
 });
 
+ipc.on('iframeReloadSize', (event,args) =>{
+  console.log('[iframeReload] ---- ','\x1b[32m','[OK]','\x1b[0m')
+  console.log('[iframeSize] ---- ',"\x1b[36m [height:" + args + "]\x1b[0m")
+})
--- Electron/Tray/resources/view-engine/render.js
+++ Electron/Tray/resources/view-engine/render.js
@@ -1,14 +1,21 @@
 const ipc = require('electron').ipcRenderer;
-const input = document.querySelector('#stringInput');
+
+const input = document.querySelector('#input');
+const iframe_qrcode = document.querySelector('#iframeqrcode');
 
 //When user press a key inside textarea, IPC send a message to index.js and run QrCodeGenerator function
 input.addEventListener('change', () => {
-    if(input.value != " " && input.value != ""){
+    if(input.value != " " && input.value != "" && input.value.length <= 250){
         ipc.send('runQrCodeGenerator',input.value)
     }
 });
 
 //Index.js IPC return a message and render catch that message and reload frame to update qrcode img
 ipc.on('QrCode200', (event,args) => {
-    if(args){window.location.reload(true);}
+    if(args){iframe_qrcode.contentDocument.location.reload(true)}
+});
+
+iframe_qrcode.addEventListener('load', () => {
+    iframe_qrcode.style.height = iframe_qrcode.contentWindow.document.body.scrollHeight + 50 + 'px';
+    ipc.send('iframeReloadSize',iframe_qrcode.style.height)
 });
